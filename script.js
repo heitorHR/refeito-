8b1bb25e5dbbb6697713871c10dc70e017e869f2
heitorHR <heitor.1213oliveira@gmail.com>
2024-03-25 08:19:56 -0300

    nono commit do css - e terceirocommit do js

--- script.js
+++ script.js
@@ -1,30 +1,18 @@
 let conta = 0
 let pessoas = 0
-
+let porcentagem = 0
 const containput=document.querySelector("#conta")
-
-
  containput.addEventListener("input",recebervalorconta)
-
-
     function recebervalorconta(evento){
         conta = number (evento.target.value)
-
-
-
         console.log(conta)
     }
-
     const pessoasinput=document.querySelector("#pessoas")
     pessoasinput.addEventListener("input",receberquantidadesdepessoas)
-
-
     function receberquantidadesdepessoas(evento){
         pessoasinput.addEventListener("input", receberquantidadesdepessoas)
         const paragrafoerro = document.querySelector(".pessoas #erro")
         const diverro = document.querySelector(".pessoas.input-box")
-        
-        
         if(evento.target.value === "0"){
             paragrafoerro.style.display ="block"
             diverro.setAttribute("id", "erro-div")
@@ -36,9 +24,21 @@ const containput=document.querySelector("#conta")
             pessoas = number(evento.target.value)
 
         }
-
-        
-
-
     }
+const botoesgorjeta = document.querySelectorAll(".gorjeta input [type = 'number']")
+botoesgorjeta.forEach(botao =>{
+    botao.addEventListener('click',receberporcentagemBotao)
 
+
+
+
+})
+function receberporcentagemBotao (evento){
+botoesgorjeta.forEach(botao => {
+    if(botao.value === evento.target.value){
+        botao.classList.add("botao-ativo")
+    }
+})
+
+  porcentagem = parseFloat(evento.target.value) /100
+}
